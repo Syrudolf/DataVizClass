b8a9b1135787c9c9a4c1daa29274c36da9feb715
Jeremy Song <syrudolf@163.com>
2021-03-27 23:16:57 +0000

    changed data

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.layout = dbc.Container([
         dbc.Col(html.H1("The Dashboard", className="text-center text-primary, mb-4"),
                 width=12)
     ]),
-
+    
     dbc.Row([
         dbc.Col([
             dcc.Dropdown(id='dropdown1', multi=False, value='Latvia',
@@ -32,8 +32,10 @@ app.layout = dbc.Container([
                                   for x in sorted(data.CountryName.unique())
                                   ]
                          ),
+            
             dcc.Graph(id='totalCases', figure={})
         ], width={'size':5, 'offset':0, 'order':1}),
+        
         
         dbc.Col([
             dcc.Dropdown(id='dropdown2', multi=True, value=['Latvia','Portugal'],
@@ -45,25 +47,14 @@ app.layout = dbc.Container([
         
     ], no_gutters=False, justify='center'),
     
+        #button
     dbc.Row([
-        dbc.Col([
-            html.H2("Header 2", style={'textdecoration':'underline'}),
-            dcc.Checklist(id='checklist', value=['Australia'],
-                          options=[{'label':x, 'value':x}
-                                  for x in sorted(data.CountryName.unique()[:10])],
-                          labelClassName='mr-3'),
-            dcc.Graph(id='plot', figure={})
-        ], width={'size':5, 'offset':0}),
-        
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody(html.P("Some text here", className='card-text')),
-                dbc.CardImg(src='https://specials-images.forbesimg.com/imageserve/1208505315/960x0.jpg?fit=scale',
-                            bottom=True) 
-            ], style={'width':'24rem'})
-        ], width={'size':5, 'offset':0})
-        
-    ], no_gutters=False, justify='center')
+        dbc.ButtonGroup(
+    [dbc.Button("Calculated daily cases"), dbc.Button("7-day-average"), dbc.Button("14-day-average")]
+)
+    ], justify='center'),
+    
+    
 ], fluid=True)
 
 # 1
@@ -88,17 +79,17 @@ def update_graph(selection):
     figure = px.line(dataS, x='Date', y='ConfirmedCases', color='CountryName')
     return figure
 
-# 3
-@app.callback(
-    Output('plot', 'figure'),
-    Input('checklist', 'value')
-)
+# # 3
+# @app.callback(
+#     Output('plot', 'figure'),
+#     Input('checklist', 'value')
+# )
 
-def update_graph(selection):
-    dataS = data[data.CountryName.isin(selection)]
-    dataS = dataS[dataS.Date == '2021-01-01']
-    figure = px.bar(dataS, x='CountryName', y='ConfirmedCases')
-    return figure
+# def update_graph(selection):
+#     dataS = data[data.CountryName.isin(selection)]
+#     dataS = dataS[dataS.Date == '2021-01-01']
+#     figure = px.bar(dataS, x='CountryName', y='ConfirmedCases')
+#     return figure
 
 if __name__=='__main__':
     app.run_server(debug=True, port=3000)
